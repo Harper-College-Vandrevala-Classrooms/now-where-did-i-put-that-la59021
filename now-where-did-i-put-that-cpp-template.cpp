eb66b1df330184149a5239b0f382d83ef2da07dd
AJ Lehmann <la59021@mail.harpercollege.edu>
2024-08-26 13:29:56 -0500

    Done

--- now-where-did-i-put-that-cpp-template.cpp
+++ now-where-did-i-put-that-cpp-template.cpp
@@ -12,15 +12,18 @@ int find(string search, char four) {
 }
 
 int find(string search, string substring) {
-    for (int i = 0; i < search.length() - substring.length(); i++) {
-        if (search[i] == substring[1]) {
-            int matching = 0;
+    if (search == substring) {
+        return 0;
+    }
+    for (int i = 0; i <= search.length() - substring.length(); i++) {
+        if (search[i] == substring[0]) {
+            int matching = 0, matchingindex = 0;
             for (int x = 0; x < substring.length(); x++) {
                 if (search[i+x] == substring[x]) {
                     matching++;
                 }
             }
-            if (matching == substring.length()) {
+            if (matching == substring.length() && i != search.length()) {
                 return i;
             }
         }
@@ -29,6 +32,31 @@ int find(string search, string substring) {
 }
 
 int main() {
-    cout << find("apple", "a");
+    string fox = "the quick brown fox jumps over the lazy dog";
+    
+    // Character search testing
+    assert(find(fox, ' ') == 3); // "Empty" Character is present
+    assert(find("thequickbrownfox", ' ') == -1); // "Empty" Character not present
+    assert(find(fox, 'a') == 36); // Character is present
+    assert(find(fox, '!') == -1); // character not present    
+
+    // String search testing 
+    assert(find(fox, "") == -1); // Empty string not present
+    assert(find("", "") == 0); // Empty string is present
+    assert(find(fox, "e") == 2); // Character string is present
+    assert(find("thequickbrownfox", "z") == -1); // Character string not present
+    assert(find(fox, "is") == -1); // whole word not present
+    assert(find(fox, "quick") == 4); // whole word is present
+    assert(find(fox, "blac") == -1); // partal word not present
+    assert(find(fox, "brown") == 10); // partal word is present
+    assert(find(fox, "jackalopes love jumping") == -1); // multi word not present
+    assert(find(fox, "the lazy dog") == 31); // multi word is present
+
+    // Example output tests
+    assert(find("The quick brown fox", 'e') == 2);
+    assert(find("The quick brown fox", "e") == 2);
+    assert(find("The quick brown fox", ' ') == 3);
+    assert(find("The quick brown fox", "quick") == 4);
+    assert(find("The quick brown fox", "quiet") == -1);
     return 1;
 }
