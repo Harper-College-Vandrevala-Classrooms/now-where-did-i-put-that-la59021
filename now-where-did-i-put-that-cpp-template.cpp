08145bd639a3f269a716acb5585249c3e04bbd93
AJ Lehmann <la59021@mail.harpercollege.edu>
2024-08-26 12:11:32 -0500

    Start

---
+++ now-where-did-i-put-that-cpp-template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int find(string search, char four) {
+    for (int i = 0; i < search.length(); i++) {
+        if (search[i] == four) {
+            return i;
+        }
+        return -1;
+    }
+}
+
+int find(string search, string substring) {
+    for (int i = 0; i < search.length() - substring.length(); i++) {
+        if (search[i] == substring[1]) {
+            int matching = 0;
+            for (int x = 0; x < substring.length(); x++) {
+                if (search[i+x] == substring[x]) {
+                    matching++;
+                }
+            }
+            if (matching == substring.length()) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    
+}
